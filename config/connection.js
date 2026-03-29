49205cd07ae3512c9fc18996b4eb1fa221e5db90
Sekouba CAMARA <sekoubacamara100@gmail.com>
2026-03-28 20:42:03 -0400

    connected to MongoDB

---
+++ config/connection.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+export default async function connectDB() {
+    try{
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log('connected to MongoDB')
+    } catch(err){
+        console.log(err)
+    }
+    
+}
+
+connectDB()
